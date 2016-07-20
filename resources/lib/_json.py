9ff36a04f4afd46ebf455830d32b4e5544d4c0d7
SLiX69 <asd2304@gmail.com>
2016-07-21 00:32:31 +0200

    update json lib
    create userdata dir if it does not exist

--- resources/lib/_json.py
+++ resources/lib/_json.py
@@ -1,6 +1,10 @@
 import simplejson as json
 import io
-import xbmc, xbmcvfs
+import xbmc, xbmcvfs, xbmcaddon
+
+addonID = "plugin.video.bookmark"
+addon = xbmcaddon.Addon(id=addonID)
+userdataDir = xbmc.translatePath(addon.getAddonInfo('profile'))
 
 log_msg = 'plugin.video.bookmark - _json_lib -'
 
@@ -25,6 +29,12 @@ def read_json(db_file):
 def write_json(db_file, data):
     xbmc.log(log_msg + '!WRITE JSON!', 1)
     xbmc.log(log_msg + 'File: ' + db_file, 1)
+    check_dir()
     with io.open(db_file, 'w', encoding='utf-8') as f:
         f.write(unicode(json.dumps(data, ensure_ascii=False)))
         f.close()
+
+
+def check_dir():
+    if not xbmcvfs.exists(userdataDir):
+        xbmcvfs.mkdir(userdataDir)
